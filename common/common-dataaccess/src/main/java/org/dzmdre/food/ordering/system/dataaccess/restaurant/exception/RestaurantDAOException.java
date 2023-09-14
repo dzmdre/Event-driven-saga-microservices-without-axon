e963c5da933d2b2e9839c237d434d37570198aad
Dmitry Drepin <lixefer@gmail.com>
2023-09-14 21:05:00 +0200

    common data access module

---
+++ common/common-dataaccess/src/main/java/org/dzmdre/food/ordering/system/dataaccess/restaurant/exception/RestaurantDAOException.java
@@ -0,0 +1,8 @@
+package org.dzmdre.food.ordering.system.dataaccess.restaurant.exception;
+
+public class RestaurantDAOException extends RuntimeException{
+
+    public RestaurantDAOException(String message) {
+        super(message);
+    }
+}
