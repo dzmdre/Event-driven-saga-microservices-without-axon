bff668d5b9aa399ac96a1845df9d1d082d9f7c3f
Dmitry Drepin <lixefer@gmail.com>
2023-09-19 14:00:26 +0200

    order messaging module refactor according outbox

--- order-service/order-messaging/src/main/java/org/dzmdre/food/ordering/system/order/service/messaging/listener/kafka/PaymentResponseKafkaListener.java
+++ order-service/order-messaging/src/main/java/org/dzmdre/food/ordering/system/order/service/messaging/listener/kafka/PaymentResponseKafkaListener.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.dzmdre.food.ordering.system.domain.valueobject.PaymentStatus;
 import org.dzmdre.food.ordering.system.kafka.consumer.KafkaConsumer;
 import org.dzmdre.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
+import org.dzmdre.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
 import org.dzmdre.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
 import org.dzmdre.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -41,15 +43,24 @@ public class PaymentResponseKafkaListener implements KafkaConsumer<PaymentRespon
                 offsets.toString());
 
         messages.forEach(paymentResponseAvroModel -> {
-            if (PaymentStatus.COMPLETED.toString().equals(paymentResponseAvroModel.getPaymentStatus().toString())) {
-                log.info("Processing successful payment for order id: {}", paymentResponseAvroModel.getOrderId());
-                paymentResponseMessageListener.paymentCompleted(orderMessagingDataMapper
-                        .paymentResponseAvroModelToPaymentResponse(paymentResponseAvroModel));
-            } else if (PaymentStatus.CANCELLED.toString().equals(paymentResponseAvroModel.getPaymentStatus().toString()) ||
-                    PaymentStatus.FAILED.toString().equals(paymentResponseAvroModel.getPaymentStatus().toString())) {
-                log.info("Processing unsuccessful payment for order id: {}", paymentResponseAvroModel.getOrderId());
-                paymentResponseMessageListener.paymentCancelled(orderMessagingDataMapper
-                        .paymentResponseAvroModelToPaymentResponse(paymentResponseAvroModel));
+            try {
+                if (PaymentStatus.COMPLETED.toString().equals(paymentResponseAvroModel.getPaymentStatus().toString())) {
+                    log.info("Processing successful payment for order id: {}", paymentResponseAvroModel.getOrderId());
+                    paymentResponseMessageListener.paymentCompleted(orderMessagingDataMapper
+                            .paymentResponseAvroModelToPaymentResponse(paymentResponseAvroModel));
+                } else if (PaymentStatus.CANCELLED.toString().equals(paymentResponseAvroModel.getPaymentStatus()) ||
+                        PaymentStatus.FAILED.toString().equals(paymentResponseAvroModel.getPaymentStatus().toString())) {
+                    log.info("Processing unsuccessful payment for order id: {}", paymentResponseAvroModel.getOrderId());
+                    paymentResponseMessageListener.paymentCancelled(orderMessagingDataMapper
+                            .paymentResponseAvroModelToPaymentResponse(paymentResponseAvroModel));
+                }
+            } catch (OptimisticLockingFailureException e) {
+                //NO-OP for optimistic lock. This means another thread finished the work, do not throw error to prevent reading the data from kafka again!
+               log.error("Caught optimistic locking exception in PaymentResponseKafkaListener for order id: {}",
+                       paymentResponseAvroModel.getOrderId());
+            } catch (OrderNotFoundException e) {
+                //NO-OP for OrderNotFoundException
+                log.error("No order found for order id: {}", paymentResponseAvroModel.getOrderId());
             }
         });
     }
